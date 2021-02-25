bf4bd33d1fd10aa351501b1b9daa0c6e11679c6a
JuanOrjuelaA <72823028+JuanOrjuelaA@users.noreply.github.com>
2021-02-25 18:14:20 -0500

    Add jwt interceptor and some styles

--- src/app/services/members.service.ts
+++ src/app/services/members.service.ts
@@ -1,14 +1,7 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../models/member';
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user'))?.token
-  })
-};
-
 @Injectable({
   providedIn: 'root'
 })
@@ -18,10 +11,10 @@ export class MembersService {
   constructor(private http: HttpClient) { }
 
   getmembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'User', httpOptions);
+    return this.http.get<Member[]>(this.baseUrl + 'User');
   }
 
   getmember(username: string) {
-    return this.http.get<Member>(this.baseUrl + 'User/' + username, httpOptions);
+    return this.http.get<Member>(this.baseUrl + 'User/' + username);
   }
 }
